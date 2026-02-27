69846a0a4dcb769409ad87e206c350edddd97508
dvlp-sy <sy8389b@gmail.com>
2026-02-27 15:46:28 +0900

    [hyodoring-1] 가족 관련 JPA Repository 클래스 생성

---
+++ src/main/java/com/v2/hyodoring/family/infrastructure/jpa/greeting/repository/GreetingCommandRepository.java
@@ -0,0 +1,9 @@
+package com.v2.hyodoring.family.infrastructure.jpa.greeting.repository;
+
+import com.v2.hyodoring.family.infrastructure.jpa.greeting.domain.GreetingEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GreetingCommandRepository extends JpaRepository<GreetingEntity,Long> {
+}
---
+++ src/main/java/com/v2/hyodoring/family/infrastructure/jpa/image/repository/ImageQueryRepository.java
@@ -0,0 +1,9 @@
+package com.v2.hyodoring.family.infrastructure.jpa.image.repository;
+
+import com.v2.hyodoring.family.infrastructure.jpa.image.domain.ImageEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImageQueryRepository extends JpaRepository<ImageEntity,Long> {
+}
